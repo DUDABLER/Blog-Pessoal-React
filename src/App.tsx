fd2a84edbd920854331ae89daff89882c2e07042
User <user@MacBook-Pro-de-User.local>
2026-05-13 21:24:05 -0300

    Feat: CRUD postagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import ListaTemas from './components/tema/listatemas/ListaTemas'
@@ -9,6 +9,9 @@ import Login from './pages/login/Login'
 import { AuthProvider } from './contexts/AuthContext'
 import FormTema from './components/tema/formtema/FormTema'
 import DeletarTema from './components/tema/deletartema/DeletarTema'
+import ListaPostagens from './components/postagem/listapostagens/ListaPostagens'
+import FormPostagem from './components/postagem/formpostagem/FormPostagem'
+import DeletarPostagem from './components/postagem/deletarpostagem/DeletarPostagem'
 
 
 function App() {
@@ -20,13 +23,19 @@ function App() {
             <Navbar/>
             <div className='min-h-[80vh]'>
               <Routes>
+                <Route path="/" element={<Navigate to="/login" />} />  
                 <Route path="/login" element={<Login />} />
                 <Route path='/home'element={<Home/>}/>
                 <Route path="/cadastro" element={<Cadastro />} />
                 <Route path="/temas" element={<ListaTemas />} />
                 <Route path="/cadastrartema" element={<FormTema />} />
                 <Route path="/editartema/:id" element={<FormTema />} />
-                <Route path="/deletartema/:id" element={<DeletarTema />} /> </Routes>
+                <Route path="/deletartema/:id" element={<DeletarTema />} /> 
+                <Route path="/postagens" element={<ListaPostagens />} />
+                <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+                <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+                <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+                </Routes>
             </div>
             <Footer/>
         </BrowserRouter>
